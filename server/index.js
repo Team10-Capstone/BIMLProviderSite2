bca32e35343f662ea28c7940c1188377cd075d35
Dylan Norris <114193369+dyl-norris19@users.noreply.github.com>
2025-04-06 19:12:40 -0500

    added endpoint to get an exercise by key, refactored index

--- server/index.js
+++ server/index.js
@@ -3,16 +3,13 @@ const app = express();
 
 //npm modules
 const cookieParser = require('cookie-parser');
-
-const fs = require("fs");
 const multer = require("multer");
 const upload = multer({dest: "uploads/"});
 
 //my modules
-const pool = require('./database');
 const auth = require('./auth');
 const graves = require('./graveyard');
-const { s3, archiver } = require('./s3');
+const s3 = require('./s3');
 
 
 require("dotenv").config({ path: '../.env' });
@@ -29,178 +26,12 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
-//uploads file given to s3 and links up with patient id given in database
-//does not require authentication (yet)
-app.post('/uploadcsv', auth.authenticateToken, upload.single("exercise"), async (req, res) => {
-    try {
-        if (!req.file)
-            return res.status(400).send("No file uploaded");
-        
-        const file = fs.readFileSync(req.file.path);
-
-        const userResult = await pool.query(
-            `SELECT patientid_fk FROM biml.login
-             WHERE useremail = $1`, [req.user.useremail]
-        );
-
-        if (userResult.rows.length === 0) 
-            return res.status(403).send("Unauthorized");
-
-        const patient_id = userResult.rows[0].patientid_fk;
-        const user = JSON.parse(req.body.userData);
-        const key = `test/${user.exercisename}_${Date.now()}.csv`;
-
-        const result = await pool.query(
-            `INSERT INTO biml.exercises(s3_key, patient_id, name)
-            VALUES ($1, $2, $3)`, [key, patient_id, user.exercisename]
-        );
-
-        await s3.putObject({
-            Body: file,
-            Bucket: "biml-bucket",
-            Key: key,
-            ContentType: 'text/csv'
-        }).promise();
-
-        fs.unlinkSync(req.file.path);
-
-        console.log("successful..?");
-
-        res.sendStatus(200);
-    } catch (err) {
-        console.log(err);
-        res.sendStatus(500);
-    }
-});
-
-//gets all exercises associated with patient id given in zip file
-//requires authentication
-app.get('/exercises', auth.authenticateToken, async (req, res) => {
-    const zipStream = archiver('zip', { zlib: {level: 9} });
-    res.attachment('exercises.zip');
-    zipStream.pipe(res);
-
-    try {
-        const user = await pool.query(
-            `SELECT patientid_fk FROM biml.login
-             WHERE useremail = $1`, [req.user.useremail]
-        );
-        if (user.rows.length === 0) 
-            return res.status(403).send("Unauthorized");
-
-        const patientId = user.rows[0].patientid_fk;
-
-        const result = await pool.query(
-            `SELECT biml.exercises.s3_key
-            FROM biml.exercises
-            WHERE patient_id = $1`, [patientId]
-        );
-
-        if (result.rows.length > 0) {
-            for (const row of result.rows) {
-                const fileStream = s3.getObject({
-                    Bucket: 'biml-bucket',
-                    Key: row.s3_key
-                }).createReadStream();
-
-                zipStream.append(fileStream, { name: row.s3_key });
-            }
-            zipStream.finalize();
-        } else {
-            res.status(404).send(":(");
-        }
-    } catch (err) {
-        console.log(err);
-        res.sendStatus(500);
-    }
-});
-
-app.get('/exercises/list', auth.authenticateToken, async (req, res) => {
-
-    try {
-        const user = await pool.query(
-            `SELECT patientid_fk FROM biml.login
-             WHERE useremail = $1`, [req.user.useremail]
-        );
-        
-        if (user.rows.length === 0) 
-            return res.status(403).send("Unauthorized");
-
-        const patientId = user.rows[0].patientid_fk;
-
-        const result = await pool.query(
-            `SELECT s3_key
-            FROM biml.exercises
-            WHERE patient_id = $1`, [patientId]
-        );
-
-        if (result.rows.length > 0) {
-            const list = result.rows.map(row => {
-                filename = row.s3_key.split('/').pop()
-                const [ key, dateString ] = filename.split('_');
-                return { key, date: new Date(parseInt(dateString)) };
-            });
-
-            res.status(200).json(list);
-        } else
-            res.sendStatus(404)
-    } catch (err) {
-        console.log(err);
-        res.sendStatus(500);
-    }
-})
-
-//downloads image from s3, later will replace with deliverable for unity app
-//requires authentication
-app.get('/app/download', auth.authenticateToken, (req, res) => {
-    try {
-        const perms = {
-            Bucket: 'biml-bucket',
-            Key: 'test/meow.jpg',
-            Expires: 60,
-            ResponseCacheControl: 'no-cache',
-            ResponseContentDisposition: 'attachment; filename="yahoooo.jpg"'
-        };
-
-        s3.getSignedUrl('getObject', perms, (err, url) => {
-            if (err)
-                return res.status(500).send('Error generating pre-signed URL');
-            res.json({ url });
-        });
-    } catch (err) {
-        console.log(err);
-        res.sendStatus(500);
-    }
-});
-
-// app.post('/uploadcsv', async (req, res) => {
-//     try {
-//         console.log('Current working directory:', process.cwd());
-//         const file = fs.readFileSync("/app/Test_Squat_Correct_Format.csv");
-
-//         await s3.putObject({
-//             Body: file,
-//             Bucket: "biml-bucket",
-//             Key: "test/ianTest.csv",
-//             ContentType: 'text/csv'
-//         }).promise();
-
-//         console.log("successful..?");
-
-//         res.sendStatus(200);
-//     } catch (err) {
-//         console.log(err);
-//         res.sendStatus(500);
-//     }
-// });
-
 //graveyard endpoints
 app.get('/therapists/patients/:id', graves.getPatientsByTherapistId)
 app.get('/patient/:id', graves.getPatientById);
 app.get('/patient/recordings/:id', graves.getPatientRecordingsById);
 app.get('/patients', graves.getPatients);
 app.get('/patients/names', graves.getPatientNames);
-
 app.post('/tests3', graves.testS3);
 
 //auth endpoints
@@ -209,6 +40,13 @@ app.post('/users/login', auth.userLogin);
 app.post('/users/token', auth.userRefreshToken);
 app.delete('/users/logout', auth.userLogout);
 app.get('/users/check', auth.userCheckToken);
+
+//s3 endpoints
+app.post('/uploadcsv', auth.authenticateToken, upload.single("exercise"), s3.uploadCSV);
+app.get('/app/download', auth.authenticateToken, s3.downloadApp);
+app.get('/exercises', auth.authenticateToken, s3.downloadExercises);
+app.get('/exercise/*', auth.authenticateToken, s3.downloadExercise)
+app.get('/exercises/list', auth.authenticateToken, s3.getExercises)
 
 //for testing purposes
 const users = [
--- server/s3.js
+++ server/s3.js
@@ -1,3 +1,6 @@
+const fs = require("fs");
+
+const pool = require('./database');
 const AWS = require("aws-sdk");
 const archiver = require('archiver');
 
@@ -9,4 +12,200 @@ const s3 = new AWS.S3({
     region: 'us-east-2'
 });
 
-module.exports = { s3, archiver};
+//uploads file given to s3 and links up with patient id given in database
+//requires authentication
+async function uploadCSV(req, res) {
+    try {
+        if (!req.file)
+            return res.status(400).send("No file uploaded");
+
+        const file = fs.readFileSync(req.file.path);
+
+        const userResult = await pool.query(
+            `SELECT patientid_fk FROM biml.login
+             WHERE useremail = $1`, [req.user.useremail]
+        );
+
+        if (userResult.rows.length === 0)
+            return res.status(403).send("Unauthorized");
+
+        const patient_id = userResult.rows[0].patientid_fk;
+        const user = JSON.parse(req.body.userData);
+        const key = `test/${user.exercisename}_${Date.now()}.csv`;
+
+        const result = await pool.query(
+            `INSERT INTO biml.exercises(s3_key, patient_id, name)
+            VALUES ($1, $2, $3)`, [key, patient_id, user.exercisename]
+        );
+
+        await s3.putObject({
+            Body: file,
+            Bucket: "biml-bucket",
+            Key: key,
+            ContentType: 'text/csv'
+        }).promise();
+
+        fs.unlinkSync(req.file.path);
+
+        console.log("successful..?");
+
+        res.sendStatus(200);
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
+}
+
+//downloads image from s3, later will replace with deliverable for unity app
+//requires authentication
+function downloadApp(req, res) {
+    try {
+        const perms = {
+            Bucket: 'biml-bucket',
+            Key: 'test/meow.jpg',
+            Expires: 60,
+            ResponseCacheControl: 'no-cache',
+            ResponseContentDisposition: 'attachment; filename="yahoooo.jpg"'
+        };
+
+        s3.getSignedUrl('getObject', perms, (err, url) => {
+            if (err)
+                return res.status(500).send('Error generating pre-signed URL');
+            res.json({ url });
+        });
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
+}
+
+//gets all exercises associated with user thats logged in
+// returns in zip file
+//requires authentication
+async function downloadExercises(req, res) {
+    const zipStream = archiver('zip', { zlib: { level: 9 } });
+    res.attachment('exercises.zip');
+    zipStream.pipe(res);
+
+    try {
+        const user = await pool.query(
+            `SELECT patientid_fk FROM biml.login
+             WHERE useremail = $1`, [req.user.useremail]
+        );
+        if (user.rows.length === 0)
+            return res.status(403).send("Unauthorized");
+
+        const patientId = user.rows[0].patientid_fk;
+
+        const result = await pool.query(
+            `SELECT biml.exercises.s3_key
+            FROM biml.exercises
+            WHERE patient_id = $1`, [patientId]
+        );
+
+        if (result.rows.length > 0) {
+            for (const row of result.rows) {
+                const fileStream = s3.getObject({
+                    Bucket: 'biml-bucket',
+                    Key: row.s3_key
+                }).createReadStream();
+
+                zipStream.append(fileStream, { name: row.s3_key });
+            }
+            zipStream.finalize();
+        } else {
+            res.status(404).send(":(");
+        }
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
+}
+
+//downloads exercise given s3 key
+//requires authentication
+async function downloadExercise(req, res) {
+    try {
+        key = req.params[0];
+
+         const userResult = await pool.query(
+            `SELECT patientid_fk FROM biml.login
+             WHERE useremail = $1`, [req.user.useremail]
+        );
+
+        if (userResult.rows.length === 0)
+            return res.status(403).send("Unauthorized");
+
+        const patientId = userResult.rows[0].patientid_fk;
+
+        const fileCheck = await pool.query(
+            `SELECT * FROM biml.exercises
+             WHERE s3_key = $1 AND patient_id = $2`, [key, patientId]
+        );
+
+        const s3Stream = s3.getObject({
+            Bucket: 'biml-bucket',
+            Key: key
+        }).createReadStream();
+
+        // Set headers so the browser knows it's a CSV file download
+        res.setHeader('Content-Disposition', `attachment; filename="${key.split('/').pop()}"`);
+        res.setHeader('Content-Type', 'text/csv');
+
+        // Pipe S3 stream directly to response
+        s3Stream.pipe(res).on('error', (err) => {
+            console.error("S3 Stream Error:", err);
+            res.sendStatus(500);
+        });
+
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
+}
+
+//gets exercise names for user thats logged in
+//requires authentication
+async function getExercises(req, res) {
+    try {
+        const user = await pool.query(
+            `SELECT patientid_fk FROM biml.login
+             WHERE useremail = $1`, [req.user.useremail]
+        );
+        
+        if (user.rows.length === 0) 
+            return res.status(403).send("Unauthorized");
+
+        const patientId = user.rows[0].patientid_fk;
+
+        const result = await pool.query(
+            `SELECT s3_key
+            FROM biml.exercises
+            WHERE patient_id = $1`, [patientId]
+        );
+
+        if (result.rows.length > 0) {
+            const list = result.rows.map(row => {
+                filename = row.s3_key.split('/').pop()
+                const [ key, dateString ] = filename.split('_');
+                return { key, date: new Date(parseInt(dateString)), exercise: row.s3_key };
+            });
+
+            res.status(200).json(list);
+        } else
+            res.sendStatus(404)
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
+}
+
+module.exports = { 
+    s3, 
+    archiver,
+    uploadCSV,
+    downloadApp,
+    downloadExercises,
+    downloadExercise,
+    getExercises
+};
